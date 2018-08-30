d5dd19d3e04ec0f776c2f91fdff392eb495a8ec4
Ruby Rubenstahl <ruby@ruby-lighting.com>
2018-08-30 11:57:40 -0700

    Initial commit of the FeathersApp provider

---
+++ src/components/FeathersApp.js
@@ -0,0 +1,44 @@
+import React from "react";
+import propTypes from "prop-types";
+const feathers = require("@feathersjs/feathers");
+const socketio = require("@feathersjs/socketio-client");
+const io = require("socket.io-client");
+
+const FeathersAppContext = React.createContext();
+
+export const withFeathersContext = Component => {
+  return props => (
+    <FeathersAppContext.Consumer>
+      {context => {
+        return <Component {...props} {...context} />;
+      }}
+    </FeathersAppContext.Consumer>
+  );
+};
+
+export class FeathersApp extends React.Component {
+  state = {
+    initialized: false,
+    connected: false
+  };
+
+  componentWillMount() {
+    const { host, port = 80 } = this.props;
+    this.socket = io(`//${host}:${port}`);
+    this.app = feathers();
+    this.app.configure(socketio(this.socket));
+  }
+
+  render() {
+    return (
+      <FeathersAppContext.Provider value={{ ...this.state, app: this.app }}>
+        {this.props.children}
+      </FeathersAppContext.Provider>
+    );
+  }
+}
+
+FeathersApp.propTypes = {
+  host: propTypes.string.isRequired,
+  port: propTypes.number
+};
