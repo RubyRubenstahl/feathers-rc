561f9e6f073c86ff9a1c98c517ecc47079cdd0fa
Ruby Rubenstahl <ruby@ruby-lighting.com>
2018-08-30 11:57:12 -0700

    Updated exports to include only what's been implemented

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { FeathersApp } from "./components/FeathersApp";
+import FeathersQuery from "./components/FeathersQuery";
+
+const ASC = 1;
+const DESC = -1;
+
+export { FeathersApp, FeathersQuery, ASC, DESC };
