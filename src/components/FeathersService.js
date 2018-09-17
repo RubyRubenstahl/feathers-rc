e9380e6ac60fd2dce80dd7d675eb0045abe1452a
Ruby Rubenstahl <ruby@ruby-lighting.com>
2018-09-17 17:37:58 -0400

    convrted FeathersService to hoc.

--- src/components/FeathersService.js
+++ src/components/FeathersService.js
@@ -3,11 +3,12 @@ import propTypes from "prop-types";
 import { withFeathersContext } from "./FeathersApp";
 
 class FeathersService extends React.Component {
-  render() {
-    const service = this.props.app.service(this.props.name);
 
-    return this.props.render
-      ? this.props.render({
+
+  render() {
+    const service =
+      this.props.app && this.props.app.service(this.props.service);
+    const childProps = {
           app: this.props.app,
           service,
           create: data => service.create(data),
@@ -15,7 +16,21 @@ class FeathersService extends React.Component {
           patch: (id, data, query) => service.patch(id, data, { query }),
           remove: (id, query) => service.remove(id, { query }),
           removeAll: () => service.remove(null)
-        })
+        }
+      
+    const childrenWithExtraProps = React.Children.map(this.props.children, child => {
+      return React.cloneElement(child, childProps);
+    });
+    
+    
+  }
+
+
+  render() {
+    const service = this.props.app.service(this.props.name);
+
+    return this.props.render
+      ? this.props.render()
       : null;
   }
 }
