c50e892df2e9965e5d116bd906764c1845d27bfd
Ruby Rubenstahl <ruby@ruby-lighting.com>
2018-09-26 16:50:41 -0400

    Creates basic FeathersApp test suite to test react-testing-library integration.

--- src/__tests__/FeathersApp.test.js
+++ src/__tests__/FeathersApp.test.js
@@ -1,5 +1,17 @@
 import React from "react";
+import { render } from "react-testing-library";
 
 import FeathersApp from "../components/FeathersApp";
+const FeathersAppTest = ({}) => {
+  return <div data-testid="feathers-app-child" />;
+};
+test("<FeathersApp/>", () => {
+  const { debug, getByTestId } = render(
+    <FeathersApp>
+      <FeathersAppTest />
+    </FeathersApp>
+  );
 
-test("<FeathersApp/>", () => {});
+  // Verify that the child component is rendered.
+  expect(getByTestId("feathers-app-child")).toBeTruthy();
+});
