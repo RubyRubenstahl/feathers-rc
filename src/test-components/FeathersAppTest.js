073dfed886f5dbdfa940d75da1a1fa131d89b3a4
Ruby Rubenstahl <ruby@ruby-lighting.com>
2018-09-27 13:17:55 -0400

    Creates FeathersAppTest component

---
+++ src/test-components/FeathersAppTest.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { withFeathersContext } from "../components/FeathersApp";
+
+export default withFeathersContext(props => {
+  const { app, host, port, initialized, connected } = props;
+
+  return (
+    <div data-testid="feathers-app-child">
+      {app && <div data-testid="app-object" />}
+      {host && <div data-testid="websocket-host">{host}</div>}
+      {port && <div data-testid={"websocket-port"}>{port}</div>}
+      {initialized && <div data-testid={"app-initialized"}>{initialized}</div>}
+      {connected && <div data-testid={"app-connected"}>{connected}</div>}
+    </div>
+  );
+});
